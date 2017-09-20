b9005edb199b35a4bd53772c0fb78413b6953899
Somtirtha Roy <somtirtha.roy@gmail.com>
2017-09-20 07:55:47 -0600

    Modified HDFFileHandler, pom, and deleted MapConroller

--- src/com/somtirtha/earthDataVisualizationTool/HDFFileHandler.java
+++ src/com/somtirtha/earthDataVisualizationTool/HDFFileHandler.java
@@ -1,5 +1,11 @@
 package com.somtirtha.earthDataVisualizationTool;
 
+import ncsa.hdf.object.Dataset;
+import ncsa.hdf.object.Datatype;
+import ncsa.hdf.object.FileFormat;
+import ncsa.hdf.object.Group;
+//import ncsa.hdf.object.h4.H4File;
+
 public class HDFFileHandler {
 	
 	public static void readHDF() {
